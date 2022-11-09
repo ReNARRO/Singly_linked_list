f77bcd8b75ea9ac0e35712cb92c314bcbef98ac2
E Dewobroto <e.dewobroto.ft21@mail.umy.ac.id>
2022-11-09 17:00:43 +0700

    menambahkan method addnode, mendeklarasikan variabel nim dan nm, dan membuat fungsi if

--- Singly_linked_list/Program.cs
+++ Singly_linked_list/Program.cs
@@ -12,10 +12,6 @@ namespace Singly_linked_list
         public int noMhs;
         public string nama;
         public Node next;
-
-        static void Main(string[] args)
-        {
-        }
     }
 
     class List
@@ -24,6 +20,33 @@ namespace Singly_linked_list
         public List()
         {
             START = null;
+        }
+
+        public void addNode() /* Method untuk menambkan sebuah Node kedalam list*/
+        {
+            int nim;
+            string nm;
+            Console.Write("\nMasukkan nomer Mahasiswa: ");
+            nim = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nMasukkan nama Mahasiswa: ");
+            nm = Console.ReadLine();
+            Node nodeBaru = new Node();
+            nodeBaru.noMhs = nim;
+            nodeBaru.nama = nm;
+
+            if(START == null || nim <= START.noMhs)/*Node ditambahkan sebagai node pertama*/
+            {
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\nNomer mahasiswa sama tidak diijinkan\n");
+                    return;
+                }
+                nodeBaru.next = START;
+                START = nodeBaru;
+                return;
+            }
+
+            /*Menemukan lokasi node baru didalam list*/
         }
     }
     
